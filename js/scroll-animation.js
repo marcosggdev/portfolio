9a2631ea4ab0511b80e5d93f4d514e3fea8e3eca
marcosggdev <marcosggdev@gmail.com>
2024-03-18 20:38:56 +0100

    Added animation of education list

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,23 +1,32 @@
 /*adds a scrolls listener that updates the margin left of the element depending on the scroll in the page*/
-function addRightScrollAnimation (element) {
-    window.addEventListener('scroll', () => {
-
-    });
-}
-
 window.addEventListener('load', () => {
 
-    var lastScrollY = 0;
     var elements = document.querySelectorAll('section#education li.education');
-
+    var lastScrollY = 0;
+    
     window.addEventListener('scroll', () => {
+        updateStates(elements, lastScrollY);
         lastScrollY = window.scrollY;
-        updatePositions(elements, lastScrollY);
     });
 
 });
 
-function updatePositions (elements, lastScrollY) {
-    //0 in the middle of the element. 100% when element dissapears
-    
+/**
+ * Updates elements state depending on window scroll
+ */
+function updateStates (elements, lastScrollY) {
+
+    for (let i = 0; i < elements.length; i++) {
+        const top = elements[i].getBoundingClientRect().top;
+        const offset = window.innerHeight / 2;
+
+        if (window.scrollY - lastScrollY > 0) {
+            //down
+            if (top - offset < 0) {
+                elements[i].classList.add('animation-scroll');
+            }
+        }
+
+    }
+
 }
